cea344635a285dd30b5b65537651158571155bc3
MVS Teja <vmatc001@odu.edu>
2021-10-29 13:51:36 -0400

    Initial Commit

---
+++ Essential-Animations/Essential_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Essential_AnimationsApp.swift
+//  Essential-Animations
+//
+//  Created by Ajay Gupta on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Essential_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
